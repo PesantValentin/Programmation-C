6a4a3d37b5285e4b89ddee573d7446ce6a76f7f9
PesantValentin <70692824+PesantValentin@users.noreply.github.com>
2021-03-25 21:15:17 +0100

    Add files via upload

---
+++ chap1tp1exo4.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <conio.h>
+
+main()
+{
+    int n;
+    int e;
+    int total;
+    total=0;
+    n=0;
+    e=0;
+
+    while(N<1)
+    {
+        printf("entrez un chiffre supérieur ou égal a 1 \n");
+        scanf("%d",&n);
+    }
+    while(e<n)
+    {
+        total++;
+        printf("%d\n",total);
+        e++;
+    }
+}
